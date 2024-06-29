1257e5aeef51642dc5112394d23cf90536f71033
Tushar Agarwal <agarwaltushar2025@gmail.com>
2024-06-29 13:12:54 +0530

    first commit

--- sever.js
+++ sever.js
@@ -2,32 +2,38 @@
 
 import express from 'express';
 const app = express();
+import mongoose from 'mongoose';
+import cookieParser from "cookie-parser";
+
+
+import authRoutes from "./Routes/auth.js"
+
+//import userRoutes from "./Routes/user.js";
+
+app.use(express.json());// bcoz req.body is null by default toh usse chnage krne k liye
+app.use(cookieParser());
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/medic').then(() => {
+    console.log("db Connected");
+});
 //const fun = require('./api');
-import run from './api.js';
+
 import cors from 'cors'
+import routes from "./Routes/Basic.js";
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+app.use(authRoutes);
 
-
-app.post("/suitabilityAssesment", async (req, res) => {
-
-
-
-    const prompt = req.body.msg;
-    console.log(req.body);
-
-    const output = await run(prompt);
-    res.send(output);
+//app.use("/api/users", userRoutes);
+app.use(routes);
 
 
 
 
 
-
-})
 
 app.listen(8000, () => {
-    console.log("setver started, Have A good Day");
+    console.log("server started, Have A good Day");
 })
